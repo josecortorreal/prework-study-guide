dd639fe28ae6f825f0d8d86724575a193830a16d
josecortorreal <josebibby23@gmail.com>
2023-01-05 03:49:12 -0500

    Added a for loop

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+// if (topic === 'HTML') {
+//   console.log("Let's study HTML!");
+// } else if (topic === 'CSS') {
+//   console.log("Let's study CSS!");
+// } else if (topic === 'Git') {
+//   console.log("Let's study Git!");
+// } else if (topic === 'JavaScript') {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log('Please try again!');
+// }
+
+
+for(var i = 0; i < topics.length; i++) {
+ console.log(topics[i]);
+}
+// console.log(topic[0]);
+// console.log(topic[1]);
+// console.log(topic[2]);
+// console.log(topic[3]);
+
+
